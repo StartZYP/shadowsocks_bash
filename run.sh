a73f2c24942251d701ef5e9d7f9fd7f4942289d6
Startzyp <asd59541511>
2019-09-17 11:12:26 +0800

    update

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
-read -p "input NodeNumber :" valnum echo $valnum
-read -p "input DB password :" val echo $val
 yum install git vim htop wget -y
 cd /root
 yum -y install python-setuptools
@@ -9,10 +7,11 @@ cd /root/shadowsocks
 pip install -r requirements.txt
 cp apiconfig.py userapiconfig.py
 cp config.json user-config.json
-sed -ig 's/{NODE}/'$valnum'/g' /root/shadowsocks/userapiconfig.py
+sed -ig 's/{NODE}/'$2'/g' /root/shadowsocks/userapiconfig.py
 sed -ig 's/{HOST}/www.ipedg.com/g' /root/shadowsocks/userapiconfig.py
 sed -ig 's/{PORT}/23306/g' /root/shadowsocks/userapiconfig.py
 sed -ig 's/{USER}/root/g' /root/shadowsocks/userapiconfig.py
-sed -ig 's/{PASS}/'$val'/g' /root/shadowsocks/userapiconfig.py
+sed -ig 's/{PASS}/'$1'/g' /root/shadowsocks/userapiconfig.py
 sed -ig 's/{DB}/ssrpanel/g' /root/shadowsocks/userapiconfig.py
 chmod +x /root/shadowsocks/run.sh
+echo "10 3 * * * root reboot" >> /etc/crontab && echo "15 3 * * * root bash /root/shadowsocks/run.sh" >> /etc/crontab && cat /etc/crontab
